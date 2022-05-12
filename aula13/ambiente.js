fc7bb5bac38803fa0d6c6d059360841772b14359
KAIQUE ALMEIDA SILVA <43797086+kaiqueealm@users.noreply.github.com>
2022-05-12 17:14:34 -0300

    atualização de site

---
+++ aula13/ambiente.js
@@ -0,0 +1,23 @@
+var c = 1
+while (c <= 50){ // <= menor ou igual a 6 
+    console.log(`Passo chata ${c}`)
+    c++
+}
+
+
+
+
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
